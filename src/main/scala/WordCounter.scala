2bf243948feff16c15f0db656a930cfab282c419
Andy Noton <andy.noton.freelancer@guardian.co.uk>
2023-05-31 20:30:19 +0100

    additional requirements

--- src/main/scala/WordCounter.scala
+++ src/main/scala/WordCounter.scala
@@ -1,32 +1,72 @@
 import scala.collection.mutable
 
-class WordCounter {
+/**
+ * This would be defined in an external translation library
+ */
+trait Translator {
+  def translate(word: String): String
+}
 
+class WordCounter(translator: Translator) {
+
+  /**
+   * A mutable Map (default implementation HashMap) is an efficient way to store and retrieve words using a hash table
+   */
   private val wordCounts = mutable.Map[String,Int]()
 
   /**
-   * method that allows you to add one or more words
+   * transforms a given word into a map key assuming that:
+   * - different case will be treated as the same word
+   * - different languages will be treated as the same word (this is a much thornier issue than the exercise suggests)
+   * @param word
+   * @return
+   */
+  private def getKey(word: String): String = {
+    translator.translate(word).toLowerCase
+  }
+
+  /**
+   * Method that allows you to add one or more words.
+   *
+   * Assumption is that zero words is not permitted, so this implementation requires at least one at compile time.
+   * Alternatively we could:
+   * - check for empty list and raise a runtime error
+   * - do nothing / call has no effect
+   * - use something like cats NonEmptyList
+   *
+   * The alphabetic character check is a naive implementation that only accepts english alphabetical characters.
+   * Depending on requirements we could use unicode letters \p{L} instead.
+   *
+   * Error handling using runtime exceptions makes the library compatible with other JVM languages such as Java.
+   * If we were using in a Scala-only context, I would probably prefer to return Either[Error, Unit] to make
+   * error processing nicer to use in Scala.
+   *
    * @param words
    */
   def addWords(word: String, moreWords: String*): Unit = {
     val words = word :: moreWords.toList
     words.foreach { word =>
-      wordCounts.addOne(word, 1 + countOccurrences(word))
+      if (!word.matches("^[a-zA-Z]+$"))
+        throw new IllegalArgumentException("Words must not contain non-alphabetical characters")
+    }
+    words.foreach { word =>
+      val key = getKey(word)
+      val count = wordCounts.getOrElse(key, 0)
+      wordCounts.addOne(key, count + 1)
     }
   }
 
   /**
-   * method that returns the count of how many times a given word was added to the word counter
+   * Method that returns the count of how many times a given word was added to the word counter.
+   *
+   * Assumes that non-english words are looked up in the same way as addWords
+   * so that:  countOccurrences("flor") === countOccurrences("flower")
+   *
    * @param word
    * @return
    */
   def countOccurrences(word: String): Int = {
-    wordCounts.getOrElse(word, 0)
+    val key = getKey(word)
+    wordCounts.getOrElse(key, 0)
   }
-}
-
-object WordCounter {
-
-  def apply() = new WordCounter
-
 }
--- src/test/scala/WordCounterTest.scala
+++ src/test/scala/WordCounterTest.scala
@@ -3,23 +3,62 @@ import org.scalatest.matchers.should.Matchers
 
 class WordCounterTest extends AnyFlatSpec with Matchers {
 
+  val naiveTranslator = new Translator {
+    override def translate(word: String): String = {
+      word.toLowerCase match {
+        case "flor" => "flower"
+        case "blume" => "flower"
+        case "grande" => "large"
+        case "gross" => "large"
+        case _ => word
+      }
+    }
+  }
+
+  def newCounter() = new WordCounter(naiveTranslator)
+
   "addWords" should "accept a single word" in {
-    val counter = WordCounter()
+    val counter = newCounter()
     counter.addWords("foo") shouldEqual {}
   }
 
   it should "accept multiple words" in {
-    val counter = WordCounter()
+    val counter = newCounter()
     counter.addWords("the", "quick", "brown", "fox") shouldEqual {}
   }
 
+  it should "NOT allow addition of words with non-alphabetic characters" in {
+    val counter = newCounter()
+    val thrown = the [IllegalArgumentException] thrownBy counter.addWords("foo2")
+    thrown.getMessage should equal ("Words must not contain non-alphabetical characters")
+  }
+
+  it should "treat same words written in different languages as the same word" in {
+    // for example if
+    // adding "flower", "flor" (Spanish word for flower) and "blume" (German word for flower)
+    // the counting method should return 3.
+    //
+    // You may assume that translation of words will be done via external class provided to you called "Translator"
+    // that will have method "translate" accepting word as an argument and it will return English name for it.
+    val counter = newCounter()
+    counter.addWords("flower", "flor", "Blume")
+    counter.countOccurrences("flower") shouldEqual 3
+    counter.countOccurrences("flor") shouldEqual 3
+    counter.countOccurrences("Blume") shouldEqual 3
+
+    counter.addWords("large", "grande", "gross")
+    counter.countOccurrences("large") shouldEqual 3
+    counter.countOccurrences("grande") shouldEqual 3
+    counter.countOccurrences("gross") shouldEqual 3
+  }
+
   "countOccurrences" should "return zero if no words were added" in {
-    val counter = WordCounter()
+    val counter = newCounter()
     counter.countOccurrences("foo") shouldEqual 0
   }
 
   it should "return number of times words were added" in {
-    val counter = WordCounter()
+    val counter = newCounter()
     counter.addWords("foo", "bar", "baz")
     counter.countOccurrences("foo") shouldEqual 1
     counter.countOccurrences("bar") shouldEqual 1
